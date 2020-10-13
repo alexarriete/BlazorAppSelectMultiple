aec60669ba77c43b4dda095b93f50e2067975196
Alejandro Cernuda <alejandro@acernuda.com>
2020-10-13 10:54:17 +0200

    Initial commit

---
+++ Data/OptionElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppSelectMultiple.Data
+{
+    public class OptionElement
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+
+        public static List<OptionElement> GetElements()
+        {
+            List<OptionElement> optionElements = new List<OptionElement>();
+            optionElements.Add(new OptionElement() { Value = 1, Name = "Madrid", Active = true });
+            optionElements.Add(new OptionElement() { Value = 2, Name = "Paris", Active = true });
+            optionElements.Add(new OptionElement() { Value = 3, Name = "Berlin", Active = true });
+            optionElements.Add(new OptionElement() { Value = 4, Name = "London", Active = true });
+            optionElements.Add(new OptionElement() { Value = 5, Name = "Rome", Active = true });
+
+            return optionElements.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
---
+++ wwwroot/scripts/AuxFuctions.js
@@ -0,0 +1,12 @@
+﻿window.ResetValue =
+{
+    GetSelectedElement: function (id) {
+        var e = document.getElementById(id);
+        var aux = e.options[e.selectedIndex].text;      
+        return aux;
+    },
+
+    SelectElement: function (element) {      
+        element.selectedIndex = 0;
+    }
+};
